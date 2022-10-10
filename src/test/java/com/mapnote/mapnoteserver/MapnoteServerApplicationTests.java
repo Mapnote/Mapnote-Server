a982f0835807ee75e280895db62ac12f0263736c
hwankim123 <hwankim1941@gmail.com>
2022-10-10 16:34:09 +0900

    [MAP-9] chore: 프로그램을 생성및 세팅

---
+++ src/test/java/com/mapnote/mapnoteserver/MapnoteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapnote.mapnoteserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapnoteServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
