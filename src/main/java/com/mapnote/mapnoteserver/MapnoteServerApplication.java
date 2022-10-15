1447490f4bee4321c786abd9d27d9a0cc2c3a0b4
beomsic <qjatjr29@khu.ac.kr>
2022-10-15 16:25:20 +0900

    [MAP-34] feat: 생성시간, 수정시간을 자동화하기위한 BaseEntity 추가

--- src/main/java/com/mapnote/mapnoteserver/MapnoteServerApplication.java
+++ src/main/java/com/mapnote/mapnoteserver/MapnoteServerApplication.java
@@ -4,7 +4,9 @@ import java.util.TimeZone;
 import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MapnoteServerApplication {
 
---
+++ src/main/java/com/mapnote/mapnoteserver/domain/common/entity/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.mapnote.mapnoteserver.domain.common.entity;
+
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity implements Serializable {
+
+  @CreatedDate
+  @Column(name = "created_at", updatable = false)
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updated_at", updatable = true)
+  private LocalDateTime updatedAt;
+
+}
