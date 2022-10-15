8b6e5eb28ecc95276c347d318f565cdc69a8327d
beomsic <qjatjr29@khu.ac.kr>
2022-10-15 15:44:17 +0900

    [MAP-34] feat: 서버의 타임존 설정(seoul)

--- src/main/java/com/mapnote/mapnoteserver/MapnoteServerApplication.java
+++ src/main/java/com/mapnote/mapnoteserver/MapnoteServerApplication.java
@@ -1,10 +1,17 @@
 package com.mapnote.mapnoteserver;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MapnoteServerApplication {
+
+	@PostConstruct
+	public void start() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MapnoteServerApplication.class, args);
