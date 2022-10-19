fc1a3c167d73d7e59e0b4c485e256d6d069be905
beomsic <qjatjr29@khu.ac.kr>
2022-10-19 17:57:14 +0900

    [MAP-26] feat: Password 암호화 클래스 추가

---
+++ src/main/java/com/mapnote/mapnoteserver/domain/user/util/PasswordEncrypter.java
@@ -0,0 +1,15 @@
+package com.mapnote.mapnoteserver.domain.user.util;
+
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
+public class PasswordEncrypter {
+
+  public static String encrypt(String password) {
+    return BCrypt.hashpw(password, BCrypt.gensalt());
+  }
+
+  public static boolean isMatch(String password, String hashedPassword) {
+    return BCrypt.checkpw(password, hashedPassword);
+  }
+
+}
