18c56024b998d5d91d91c317a883996260975d09
beomsic <qjatjr29@khu.ac.kr>
2022-10-19 18:00:40 +0900

    [MAP-26] feat: 비밀번호 수정 api 추가

--- src/main/java/com/mapnote/mapnoteserver/domain/user/controller/UserController.java
+++ src/main/java/com/mapnote/mapnoteserver/domain/user/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -60,6 +61,12 @@ public class UserController {
   public ResponseEntity<UserResponse.UserDetailResponse> getUserDetail(@PathVariable UUID userId) {
     UserResponse.UserDetailResponse userDetail = userService.getUserDetail(userId);
     return ResponseEntity.ok(userDetail);
+  }
+
+  @PutMapping("/newPassword")
+  public ResponseEntity<UserResponse.UserDetailResponse> changePassword(@Validated @RequestBody UserRequest.NewPassword passwordRequest) {
+    userService.changePassword(passwordRequest);
+    return ResponseEntity.ok().build();
   }
 
 }
