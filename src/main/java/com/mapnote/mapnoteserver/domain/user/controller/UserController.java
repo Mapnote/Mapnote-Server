d18f8c6e6feaa1db27f65fcab6fdddfdcbc76c3e
beomsic <qjatjr29@khu.ac.kr>
2022-10-20 16:50:20 +0900

    [MAP-27] feat: 유저 삭제 api 추가

--- src/main/java/com/mapnote/mapnoteserver/domain/user/controller/UserController.java
+++ src/main/java/com/mapnote/mapnoteserver/domain/user/controller/UserController.java
@@ -8,6 +8,7 @@ import java.net.URI;
 import java.util.UUID;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -67,6 +68,12 @@ public class UserController {
   public ResponseEntity<UserResponse.UserDetailResponse> changePassword(@Validated @RequestBody UserRequest.NewPassword passwordRequest) {
     userService.changePassword(passwordRequest);
     return ResponseEntity.ok().build();
+  }
+
+  @DeleteMapping("/{userId}")
+  public ResponseEntity<Void> delete(@PathVariable UUID userId) {
+    userService.delete(userId);
+    return ResponseEntity.noContent().build();
   }
 
 }
