1de13a397bcb132eca3a34f604e529db3982e4a5
beomsic <qjatjr29@khu.ac.kr>
2022-10-20 19:29:05 +0900

    [MAP-22] feat: 사용자 토큰 확인을 위한 AOP, 어노테이션 기능 추가

---
+++ src/main/java/com/mapnote/mapnoteserver/security/aop/AuthAspect.java
@@ -0,0 +1,39 @@
+package com.mapnote.mapnoteserver.security.aop;
+
+import com.mapnote.mapnoteserver.domain.common.exception.NotFoundException;
+import com.mapnote.mapnoteserver.domain.user.repository.UserRepository;
+import com.mapnote.mapnoteserver.security.jwt.JwtTokenProvider;
+import javax.servlet.http.HttpServletRequest;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+
+@Aspect
+@Component
+public class AuthAspect {
+
+  private static final String HEADER = "Authorization";
+
+  private final JwtTokenProvider jwtTokenProvider;
+  private final UserRepository userRepository;
+  private final HttpServletRequest servletRequest;
+
+  public AuthAspect(JwtTokenProvider jwtTokenProvider,
+      UserRepository userRepository, HttpServletRequest servletRequest) {
+    this.jwtTokenProvider = jwtTokenProvider;
+    this.userRepository = userRepository;
+    this.servletRequest = servletRequest;
+  }
+
+  @Around("@annotation(com.mapnote.mapnoteserver.security.aop.Auth)")
+  public void validateToken() {
+    String token = servletRequest.getHeader(HEADER);
+
+    String userEmail = jwtTokenProvider.getUserEmail(token);
+    userRepository.findByEmail(userEmail)
+        .orElseThrow(() -> new HttpClientErrorException(HttpStatus.UNAUTHORIZED));
+  }
+
+}
