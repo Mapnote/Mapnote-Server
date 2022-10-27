c74acc6c829b8ba4d6dd066c8f30f96da899b5eb
beomsic <qjatjr29@khu.ac.kr>
2022-10-27 16:08:02 +0900

    :sparkles: [MAP-58] feat: 각 Request, Response 에 스웨거 설정

--- src/main/java/com/mapnote/mapnoteserver/domain/user/dto/UserRequest.java
+++ src/main/java/com/mapnote/mapnoteserver/domain/user/dto/UserRequest.java
@@ -1,5 +1,6 @@
 package com.mapnote.mapnoteserver.domain.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import lombok.Getter;
@@ -9,23 +10,28 @@ public class UserRequest {
 
   @Getter
   public static class SignUp {
+    @Schema(description = "이메일", example = "test@gmail.com")
     @NotEmpty(message = "이메일은 필수 입력해야합니다.")
     @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
     private String email;
 
+    @Schema(description = "비밀번호", example = "test123!!")
     @NotEmpty(message = "비밀번호는 필수 입력해야 합니다.")
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
 
+    @Schema(description = "이름(닉네임)", example = "beomsic")
     private String name;
   }
 
   @Getter
   public static class Login {
+    @Schema(description = "이메일", example = "test@gmail.com")
     @NotEmpty(message = "이메일은 필수 입력해야합니다.")
     @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
     private String email;
 
+    @Schema(description = "비밀번호", example = "test123!!")
     @NotEmpty(message = "비밀번호는 필수 입력해야 합니다.")
     private String password;
 
@@ -36,24 +42,29 @@ public class UserRequest {
 
   @Getter
   public static class Reissue {
+    @Schema(description = "JWT AT", example = "eyJhbGciOiJIUzI1NiJ9.eyJhdXRoS2V5IjoiaGhxamF0pow3QqLTcL7l")
     @NotEmpty(message = "accessToken 값을 입력해야 합니다.")
     private String accessToken;
 
+    @Schema(description = "JWT RT", example = "eyJhbGciOiJIUzI1NiJ9.-eyJhdXRoS2V5Ipow3QqLTcL7lU")
     @NotEmpty(message = "refreshToken 값을 입력해야 합니다.")
     private String refreshToken;
   }
 
   @Getter
   public static class Logout {
+    @Schema(description = "JWT AT", example = "eyJhbGciOiJIUzI1NiJ9.eyJhdXRoS2V5IjoiaGhxamF0pow3QqLTcL7lU")
     @NotEmpty()
     private String accessToken;
 
+    @Schema(description = "JWT RT", example = "eyJhbGciOiJIUzI1NiJ9.eyJhdXRoS2V5Ipow3QqLTcL7lU")
     @NotEmpty()
     private String refreshToken;
   }
 
   @Getter
   public static class Email {
+    @Schema(description = "이메일", example = "test@gmail.com")
     @NotEmpty(message = "이메일은 필수값 입니다.")
     @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
     private String email;
@@ -61,14 +72,13 @@ public class UserRequest {
 
   @Getter
   public static class NewPassword {
-//    @NotEmpty(message = "이메일은 필수값 입니다.")
-//    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
-//    private String email;
 
+    @Schema(description = "기존 비밀번호", example = "test123!!")
     @NotEmpty(message = "기존 비밀번호는 필수 입력해야 합니다.")
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$", message = "기존 비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String oldPassword;
 
+    @Schema(description = "새로운 비밀번호", example = "test123!!")
     @NotEmpty(message = "새로운 비밀번호는 필수 입력해야 합니다.")
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$", message = "바꿀 비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String newPassword;
@@ -76,19 +86,22 @@ public class UserRequest {
 
   @Getter
   public static class ChangeInfo {
+    @Schema(description = "이름(닉네임)", example = "beomsic")
     private String name;
 
+    @Schema(description = "반경", example = "8")
     private Long boundary;
 
   }
 
   @Getter
   public static class RenewalPassword {
-
+    @Schema(description = "이메일", example = "test@gmail.com")
     @NotEmpty(message = "이메일은 필수값 입니다.")
     @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
     private String email;
 
+    @Schema(description = "새로운 비밀번호", example = "test123!!")
     @NotEmpty(message = "새로운 비밀번호는 필수 입력해야 합니다.")
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$", message = "바꿀 비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String newPassword;
