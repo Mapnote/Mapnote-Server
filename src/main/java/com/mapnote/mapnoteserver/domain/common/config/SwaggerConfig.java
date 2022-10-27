6a4a2884446c23e4fcd5c21027cd32600040ef2c
beomsic <qjatjr29@khu.ac.kr>
2022-10-27 13:38:42 +0900

    :sparkles: [MAP-57] feat: Swagger configuration 추가

---
+++ src/main/java/com/mapnote/mapnoteserver/domain/common/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.mapnote.mapnoteserver.domain.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public GroupedOpenApi userApi() {
+    return GroupedOpenApi.builder()
+        .group("유저 관련 API")
+        .pathsToMatch("/api/v1/users/**")
+        .build();
+  }
+
+  @Bean
+  public GroupedOpenApi mailApi() {
+    return GroupedOpenApi.builder()
+        .group("메일 관련 API")
+        .pathsToMatch("/api/v1/mail/**")
+        .build();
+  }
+
+  @Bean
+  public OpenAPI springShopOpenAPI() {
+    return new OpenAPI()
+        .info(new Info().title("Mapnote SWAGGER")
+            .description("Mapnote Swagger - API 문서"));
+  }
+}
