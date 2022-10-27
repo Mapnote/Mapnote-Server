7435325484834da1713233513dd6f23692eccb4c
myeongju-kim <mjoo1106@naver.com>
2022-10-27 16:37:48 +0900

    [MAP-41] fix: modify delete service function

--- src/main/java/com/mapnote/mapnoteserver/domain/memo/service/MemoService.java
+++ src/main/java/com/mapnote/mapnoteserver/domain/memo/service/MemoService.java
@@ -2,49 +2,44 @@ package com.mapnote.mapnoteserver.domain.memo.service;
 
 
 
+import com.mapnote.mapnoteserver.domain.common.exception.ErrorCode;
+import com.mapnote.mapnoteserver.domain.common.exception.NotFoundException;
 import com.mapnote.mapnoteserver.domain.memo.dto.MemoRequest;
 import com.mapnote.mapnoteserver.domain.memo.entity.Memo;
 
 import com.mapnote.mapnoteserver.domain.memo.repository.MemoRepository;
-import com.mapnote.mapnoteserver.domain.user.entity.Authority;
 import com.mapnote.mapnoteserver.domain.user.entity.User;
 import com.mapnote.mapnoteserver.domain.user.repository.UserRepository;
-import com.mapnote.mapnoteserver.domain.user.util.PasswordEncrypter;
-import com.mapnote.mapnoteserver.domain.user.util.UserConverter;
-import com.mapnote.mapnoteserver.security.jwt.JwtExpiration;
-import com.mapnote.mapnoteserver.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
-
-import static org.springframework.util.ObjectUtils.isEmpty;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MemoService {
+  private final UserRepository userRepository;
   private final MemoRepository memoRepository;
-  private final AuthenticationManagerBuilder authenticationManagerBuilder;
-  private final JwtTokenProvider jwtTokenProvider;
-  private final RedisTemplate redisTemplate;
-
-  private static final String BEARER_TYPE = "Bearer";
 
   @Transactional
-  public Long memoSave(MemoRequest.Save memoSave) {
+  public Long memoSave(MemoRequest.Save memoSave, UUID uuid) {
+    User user=userRepository.findById(uuid).orElse(null);
     Memo memo=Memo.builder()
+            .user(user)
             .contents(memoSave.getContents())
             .type(memoSave.getType())
             .build();
     memoRepository.save(memo);
     return memo.getId();
   }
+  @Transactional
+  public Long memoDelete(MemoRequest.Delete memoDelete) {
+    Memo memo = memoRepository.findById(memoDelete.getId())
+            .orElseThrow(() -> new NotFoundException("해당 유저가 존재하지 않습니다.", ErrorCode.NOT_FOUND_MEMO));
 
-
+    memoRepository.delete(memo);
+    return memo.getId();
+  }
 
 }
