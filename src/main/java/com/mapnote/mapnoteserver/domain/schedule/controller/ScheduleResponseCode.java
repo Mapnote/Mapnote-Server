5bb712cfc7ac7f459f82223cb681b8e594254a46
beomsic <qjatjr29@khu.ac.kr>
2022-11-08 13:45:11 +0900

    :sparkles: [MAP-50] feat: 스케줄 response enum 추가

---
+++ src/main/java/com/mapnote/mapnoteserver/domain/schedule/controller/ScheduleResponseCode.java
@@ -0,0 +1,21 @@
+package com.mapnote.mapnoteserver.domain.schedule.controller;
+
+import com.mapnote.mapnoteserver.domain.common.dto.ResponseCode;
+import lombok.Getter;
+
+@Getter
+public enum ScheduleResponseCode implements ResponseCode  {
+
+  CREATE_SUCCESS(201, "S201", "Signup SUCCESS")
+  ;
+
+  private String code;
+  private String message;
+  private int status;
+
+  ScheduleResponseCode(int status, String code, String message) {
+    this.code = code;
+    this.message = message;
+    this.status = status;
+  }
+}
