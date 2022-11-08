fa97dce073076789b2d84f8b80e0eb020747f294
beomsic <qjatjr29@khu.ac.kr>
2022-11-08 15:35:37 +0900

    :sparkles: [MAP-50] feat: 유저 아이디를 통한 유저의 스케줄 리스트 API 추가

--- src/main/java/com/mapnote/mapnoteserver/domain/schedule/controller/ScheduleController.java
+++ src/main/java/com/mapnote/mapnoteserver/domain/schedule/controller/ScheduleController.java
@@ -4,15 +4,19 @@ import com.mapnote.mapnoteserver.domain.common.dto.DataResponse;
 import com.mapnote.mapnoteserver.domain.schedule.dto.ScheduleRequest;
 import com.mapnote.mapnoteserver.domain.schedule.dto.ScheduleResponse;
 import com.mapnote.mapnoteserver.domain.schedule.dto.ScheduleResponse.ScheduleDetail;
+import com.mapnote.mapnoteserver.domain.schedule.dto.ScheduleResponse.ScheduleSummary;
+import com.mapnote.mapnoteserver.domain.schedule.entity.ScheduleStatus;
 import com.mapnote.mapnoteserver.domain.schedule.service.ScheduleService;
-import com.mapnote.mapnoteserver.domain.user.controller.UserResponseCode;
-import com.mapnote.mapnoteserver.domain.user.dto.UserResponse;
-import com.mapnote.mapnoteserver.domain.user.dto.UserResponse.UserSignupResponse;
 import com.mapnote.mapnoteserver.security.CustomUserDetails;
+import com.mapnote.mapnoteserver.security.aop.Auth;
 import com.mapnote.mapnoteserver.security.aop.CurrentUser;
 import java.net.URI;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +33,7 @@ public class ScheduleController {
     this.scheduleService = scheduleService;
   }
 
+  @Auth
   @PostMapping("")
   public ResponseEntity<DataResponse<ScheduleResponse.ScheduleDetail>> createSchedule(@CurrentUser CustomUserDetails user, @RequestBody
       ScheduleRequest.Create create) {
@@ -38,5 +43,19 @@ public class ScheduleController {
     return ResponseEntity.created(URI.create("/")).body(response);
   }
 
+  @Auth
+  @GetMapping("/{status}")
+  public ResponseEntity<DataResponse<Slice<ScheduleSummary>>> getScheduleList(@CurrentUser CustomUserDetails user,
+      @PathVariable String status,
+      @PageableDefault(page = 0, size = 5) Pageable pageable) {
+
+    ScheduleStatus scheduleStatus = ScheduleStatus.valueOf(status.toUpperCase());
+
+    Slice<ScheduleSummary> summaryList = scheduleService.findScheduleList(user.getId(), pageable, scheduleStatus);
+
+    DataResponse<Slice<ScheduleSummary>> response = new DataResponse<>(ScheduleResponseCode.GET_SCHEDULE_LIST, summaryList);
+
+    return ResponseEntity.ok(response);
+  }
 
 }
