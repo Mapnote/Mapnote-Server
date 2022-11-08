23d5eae34c10be3944c775eeb903faaf9ba5c7ad
beomsic <qjatjr29@khu.ac.kr>
2022-11-08 15:35:08 +0900

    :sparkles: [MAP-50] feat: 유저 아이디와 스케줄 상태에 따른 조회 기능 추가

--- src/main/java/com/mapnote/mapnoteserver/domain/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/mapnote/mapnoteserver/domain/schedule/repository/ScheduleRepository.java
@@ -1,9 +1,17 @@
 package com.mapnote.mapnoteserver.domain.schedule.repository;
 
+import com.mapnote.mapnoteserver.domain.schedule.entity.ScheduleStatus;
 import com.mapnote.mapnoteserver.domain.schedule.entity.Schedules;
+import java.util.Optional;
 import java.util.UUID;
+import javax.websocket.server.PathParam;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface ScheduleRepository extends JpaRepository<Schedules, UUID> {
+public interface ScheduleRepository extends JpaRepository<Schedules, Long> {
+
+  Slice<Schedules> findByUser_IdAndScheduleStatusOrderByCreatedAtDesc(UUID userId, ScheduleStatus scheduleStatus, Pageable pageable);
 
 }
