66ac691242a3a64a9b1004689e35fc212e08391d
beomsic <qjatjr29@khu.ac.kr>
2022-11-08 15:33:45 +0900

    :sparkles: [MAP-50] feat: 유저의 스케줄 리스트 전체 조회 로직 추가

--- src/main/java/com/mapnote/mapnoteserver/domain/schedule/service/ScheduleService.java
+++ src/main/java/com/mapnote/mapnoteserver/domain/schedule/service/ScheduleService.java
@@ -4,12 +4,16 @@ import com.mapnote.mapnoteserver.domain.common.exception.ErrorCode;
 import com.mapnote.mapnoteserver.domain.common.exception.NotFoundException;
 import com.mapnote.mapnoteserver.domain.schedule.dto.ScheduleRequest.Create;
 import com.mapnote.mapnoteserver.domain.schedule.dto.ScheduleResponse.ScheduleDetail;
+import com.mapnote.mapnoteserver.domain.schedule.dto.ScheduleResponse.ScheduleSummary;
+import com.mapnote.mapnoteserver.domain.schedule.entity.ScheduleStatus;
 import com.mapnote.mapnoteserver.domain.schedule.entity.Schedules;
 import com.mapnote.mapnoteserver.domain.schedule.repository.ScheduleRepository;
 import com.mapnote.mapnoteserver.domain.schedule.util.ScheduleConverter;
 import com.mapnote.mapnoteserver.domain.user.entity.User;
 import com.mapnote.mapnoteserver.domain.user.repository.UserRepository;
 import java.util.UUID;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,14 +31,20 @@ public class ScheduleService {
     this.userRepository = userRepository;
   }
 
+  @Transactional
+  public ScheduleDetail create(UUID userId, Create create) {
 
-  public ScheduleDetail create(UUID id, Create create) {
-
-    User user = userRepository.findById(id)
+    User user = userRepository.findById(userId)
         .orElseThrow(() -> new NotFoundException("유저가 존재하지 않습니다.", ErrorCode.NOT_FOUND_USER));
 
-    Schedules newSchedule = scheduleRepository.save(ScheduleConverter.toSchedule(create, user));
+    Schedules schedule = scheduleRepository.save(ScheduleConverter.toSchedule(create, user));
 
-    return ScheduleConverter.toDetail(newSchedule);
+    return ScheduleConverter.toDetail(schedule);
+  }
+
+  public Slice<ScheduleSummary> findScheduleList(UUID userId, Pageable pageable, ScheduleStatus scheduleStatus) {
+    Slice<Schedules> schedulesList = scheduleRepository.findByUser_IdAndScheduleStatusOrderByCreatedAtDesc(userId, scheduleStatus, pageable);
+
+    return schedulesList.map(ScheduleConverter::toSummary);
   }
 }
