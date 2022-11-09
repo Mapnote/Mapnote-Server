f37bc926cada657ccda141061b504f477d71a083
beomsic <qjatjr29@khu.ac.kr>
2022-11-09 20:31:44 +0900

    :sparkles: [MAP-60] feat: 스케줄 관련 스웨거 설정

--- src/main/java/com/mapnote/mapnoteserver/domain/schedule/controller/ScheduleController.java
+++ src/main/java/com/mapnote/mapnoteserver/domain/schedule/controller/ScheduleController.java
@@ -1,16 +1,22 @@
 package com.mapnote.mapnoteserver.domain.schedule.controller;
 
 import com.mapnote.mapnoteserver.domain.common.dto.DataResponse;
+import com.mapnote.mapnoteserver.domain.common.dto.ErrorResponse;
 import com.mapnote.mapnoteserver.domain.schedule.dto.ScheduleRequest;
 import com.mapnote.mapnoteserver.domain.schedule.dto.ScheduleResponse;
 import com.mapnote.mapnoteserver.domain.schedule.dto.ScheduleResponse.ScheduleDetail;
 import com.mapnote.mapnoteserver.domain.schedule.dto.ScheduleResponse.ScheduleSummary;
 import com.mapnote.mapnoteserver.domain.schedule.entity.ScheduleStatus;
 import com.mapnote.mapnoteserver.domain.schedule.service.ScheduleService;
-import com.mapnote.mapnoteserver.domain.user.controller.UserResponseCode;
 import com.mapnote.mapnoteserver.security.CustomUserDetails;
 import com.mapnote.mapnoteserver.security.aop.Auth;
 import com.mapnote.mapnoteserver.security.aop.CurrentUser;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.net.URI;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
@@ -26,6 +32,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "schedule", description = "스케줄 API")
 @RestController
 @RequestMapping("/api/v1/schedule")
 public class ScheduleController {
@@ -37,6 +44,14 @@ public class ScheduleController {
     this.scheduleService = scheduleService;
   }
 
+  @Operation(summary = "create schedule", description = "스케줄을 생성")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "OK"),
+      @ApiResponse(responseCode = "400", description = "BAD REQUEST", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "401", description = "UN AUTHORIZATION", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "404", description = "NOT FOUND", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+  })
   @Auth
   @PostMapping("")
   public ResponseEntity<DataResponse<ScheduleResponse.ScheduleDetail>> createSchedule(@CurrentUser CustomUserDetails user, @RequestBody
@@ -47,6 +62,14 @@ public class ScheduleController {
     return ResponseEntity.created(URI.create("/")).body(response);
   }
 
+  @Operation(summary = "get schedule list", description = "스케줄 리스트 조회")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "OK"),
+      @ApiResponse(responseCode = "400", description = "BAD REQUEST", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "401", description = "UN AUTHORIZATION", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "404", description = "NOT FOUND", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+  })
   @Auth
   @GetMapping("/list/{status}")
   public ResponseEntity<DataResponse<Slice<ScheduleSummary>>> getScheduleList(@CurrentUser CustomUserDetails user,
@@ -62,6 +85,14 @@ public class ScheduleController {
     return new ResponseEntity<>(response, HttpStatus.valueOf(response.getStatus()));
   }
 
+  @Operation(summary = "get schedule detail", description = "스케줄 정보 상세 조회")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "OK"),
+      @ApiResponse(responseCode = "400", description = "BAD REQUEST", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "401", description = "UN AUTHORIZATION", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "404", description = "NOT FOUND", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+  })
   @Auth
   @GetMapping("/{scheduleId}")
   public ResponseEntity<DataResponse<ScheduleResponse.ScheduleDetail>> getScheduleDetail(@CurrentUser CustomUserDetails user, @PathVariable Long scheduleId) {
@@ -72,6 +103,14 @@ public class ScheduleController {
     return new ResponseEntity<>(response, HttpStatus.valueOf(response.getStatus()));
   }
 
+  @Operation(summary = "toggle schedule status", description = "스케줄의 진행중/완료 상태 변경")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "OK"),
+      @ApiResponse(responseCode = "400", description = "BAD REQUEST", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "401", description = "UN AUTHORIZATION", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "404", description = "NOT FOUND", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+  })
   @Auth
   @PutMapping("/status/{scheduleId}")
   public ResponseEntity<DataResponse<ScheduleResponse.ScheduleDetail>> toggleScheduleStatus(@CurrentUser CustomUserDetails user, @PathVariable Long scheduleId) {
@@ -82,6 +121,14 @@ public class ScheduleController {
     return new ResponseEntity<>(response, HttpStatus.valueOf(response.getStatus()));
   }
 
+  @Operation(summary = "update schedule info", description = "스케줄 정보 변경(내용, 장소)")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "OK"),
+      @ApiResponse(responseCode = "400", description = "BAD REQUEST", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "401", description = "UN AUTHORIZATION", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "404", description = "NOT FOUND", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+  })
   @Auth
   @PutMapping("/{scheduleId}")
   public ResponseEntity<DataResponse<ScheduleResponse.ScheduleDetail>> updateSchedule(@CurrentUser CustomUserDetails user,
@@ -93,6 +140,14 @@ public class ScheduleController {
     return new ResponseEntity<>(response, HttpStatus.valueOf(response.getStatus()));
   }
 
+  @Operation(summary = "delete schedule", description = "스케줄 삭제")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "OK"),
+      @ApiResponse(responseCode = "400", description = "BAD REQUEST", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "401", description = "UN AUTHORIZATION", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "404", description = "NOT FOUND", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+  })
   @Auth
   @DeleteMapping("/{scheduleId}")
   public ResponseEntity<DataResponse<Void>> delete(@CurrentUser CustomUserDetails user, @PathVariable Long scheduleId) {
