2c7027b11016bb5634992c4cc86543d4fd9b97ef
beomsic <qjatjr29@khu.ac.kr>
2022-11-17 14:16:27 +0900

    :recycle:  [MAP-71] refactor: 사용하지 않은 import 삭제

--- src/main/java/com/mapnote/mapnoteserver/domain/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/mapnote/mapnoteserver/domain/schedule/repository/ScheduleRepository.java
@@ -4,11 +4,9 @@ import com.mapnote.mapnoteserver.domain.schedule.entity.ScheduleStatus;
 import com.mapnote.mapnoteserver.domain.schedule.entity.Schedules;
 import java.util.Optional;
 import java.util.UUID;
-import javax.websocket.server.PathParam;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface ScheduleRepository extends JpaRepository<Schedules, Long> {
 
