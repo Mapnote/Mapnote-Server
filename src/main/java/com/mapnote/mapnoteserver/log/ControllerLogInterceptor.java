2cea927e420f2fffe9569bcbf9910e99853f255e
beomsic <qjatjr29@khu.ac.kr>
2022-11-23 15:19:14 +0900

    :recycle: [MAP-75] refactor: 사용하지 않은 출력함수 제거

--- src/main/java/com/mapnote/mapnoteserver/log/ControllerLogInterceptor.java
+++ src/main/java/com/mapnote/mapnoteserver/log/ControllerLogInterceptor.java
@@ -25,7 +25,6 @@ public class ControllerLogInterceptor extends HandlerInterceptorAdapter {
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
       throws Exception {
-    System.out.println("hihihihi");
     ControllerLog controllerLog = new ControllerLog(
         request.getMethod(),
         (String) request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE),
@@ -34,7 +33,6 @@ public class ControllerLogInterceptor extends HandlerInterceptorAdapter {
 
     log.info(controllerLog.toString());
     controllerLogFluxSink.next(controllerLog);
-
     return super.preHandle(request, response, handler);
   }
 }
