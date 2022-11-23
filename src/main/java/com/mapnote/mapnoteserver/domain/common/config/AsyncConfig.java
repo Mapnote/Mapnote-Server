9c3ef9353c2d01bd58d5f0e59d8cd5b6945636c7
beomsic <qjatjr29@khu.ac.kr>
2022-11-23 18:26:59 +0900

    :sparkles: [MAP-75] feat: 슬랙 알림 기능을 별도 스레드에서 처리하기 위한 config 설정

---
+++ src/main/java/com/mapnote/mapnoteserver/domain/common/config/AsyncConfig.java
@@ -0,0 +1,30 @@
+package com.mapnote.mapnoteserver.domain.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+
+  private static int CORE_POOL_SIZE = 15;
+  private static int MAX_POOL_SIZE = 25;
+  private static int QUEUE_CAPACITY = 10;
+  private static String THREAD_NAME_PREFIX = "async-task";
+
+  @Bean
+  public TaskExecutor createTaskExecutor() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
+    executor.setCorePoolSize(CORE_POOL_SIZE);
+    executor.setMaxPoolSize(MAX_POOL_SIZE);
+    executor.setQueueCapacity(QUEUE_CAPACITY);
+    executor.setThreadNamePrefix(THREAD_NAME_PREFIX);
+    executor.initialize();
+
+    return executor;
+  }
+}
