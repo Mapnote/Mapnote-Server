0fb07fc694b05d7486a5fe5847653291b18b6431
myeongju-kim <mjoo1106@naver.com>
2022-12-04 11:38:22 +0900

    [MAP-65] fix: add setter annotation

--- src/main/java/com/mapnote/mapnoteserver/domain/schedule/entity/Schedules.java
+++ src/main/java/com/mapnote/mapnoteserver/domain/schedule/entity/Schedules.java
@@ -24,6 +24,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
@@ -32,6 +33,7 @@ import org.hibernate.annotations.Where;
 @Where(clause = "is_deleted = false")
 @SQLDelete(sql = "UPDATE schedules SET is_deleted = true WHERE id = ?")
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
